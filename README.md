f43b8d0d0b0e1937b892b6e07fc26317c8820542
Ruslan Abdrahmanov <ruslan.abdrahmanov@tatar.ru>
2017-02-08 09:36:07 +0300

    Reworked to OpenSSH file

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# PuTTY-to-Shuttle
+# PuTTY-to-OpenSSH
 
-Convert your exported list of PuTTY accounts to a Shuttle configuration file.
+Convert your exported list of PuTTY accounts to a OpenSSH configuration file.
 
 ## Rationale: For those who SSH.
 
-If you're moving from Windows to Mac you'll probably want to keep all your SSH accounts you've been using in [PuTTY](http://www.chiark.greenend.org.uk/~sgtatham/putty/).
+If you're moving from Windows to Linux you'll probably want to keep all your SSH accounts you've been using in [PuTTY](http://www.chiark.greenend.org.uk/~sgtatham/putty/).
 
-On a Mac [Shuttle](http://fitztrev.github.io/shuttle/) will be the simplest tool help you keep track of all your SSH accounts.
+On a Linux [OpenSSH](https://www.openssh.com/) will be the simplest tool help you keep track of all your SSH accounts.
 
-To speed up the migration convert your list of accounts from PuTTY registery key to a Shuttle format which is in JSON.
+To speed up the migration convert your list of accounts from PuTTY registery key to a OpenSSH configuration file format.
 
 ## Windows: Export Registry Keys
 
@@ -18,23 +18,31 @@ The below command should export all your sessions to the a file `putty.reg` on y
 regedit /e "%userprofile%\desktop\putty.reg" HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\Sessions
 ```
 
-## Mac: Convert and Import to Shuttle
+## Linux: Convert and Import to OpenSSH
 
-1. Copy your exported PuTTY sessions to your Mac running Shuttle.
-2. Place the `putty.reg` file in the same directory as the `convert.php` script. 
-3. Run the `convert.php` in Terminal to generate a `shuttle.json` file. 
+1. Copy your exported PuTTY sessions to your Linux (with installed php).
+2. Place the `putty.reg` file in the same directory as the `convert.php` script.
+3. Run the `convert.php` in Terminal to generate a OpenSSH configuration file.
 
 ```
-php convert.php putty.reg shuttle.json
+php convert.php putty.reg ssh_config
 ```
 
-4. Copy the `shuttle.json` file to `~/.shuttle.json` and check Shuttle.
+4. Copy the `ssh_config` file to `~/.ssh/config`.
 ```
-cp ./shuttle.json ~/.shuttle.json
+cp ./ssh_config ~/.ssh/config
 ```
+
+## Processed fields
+
+ - HostName
+ - PortNumber
+ - UserName
 
 ## Links
 
-https://github.com/fitztrev/shuttle
+https://www.openssh.com/
+
+https://www.opennet.ru/cgi-bin/opennet/man.cgi?topic=ssh&category=1
 
 http://www.chiark.greenend.org.uk/~sgtatham/putty/
--- convert.php
+++ convert.php
@@ -1,22 +1,20 @@
 <?php
 /**
- * PuTTY-to-Shuttle Converter
+ * PuTTY-to-OpenSSH Converter
  */
 
 header( 'content-type: text/plain; charset=utf-8' );
 
-if ( PHP_SAPI !== 'cli' )
-{
+if ( PHP_SAPI !== 'cli' ) {
     echo 'This is meant to be run on the command line.' . PHP_EOL;
     exit( 1 );
-};
+}
 
-if ( count( $argv ) !== 3 )
-{
+if ( count( $argv ) !== 3 ) {
     echo 'Incorrect number of arguments.' . PHP_EOL;
-    echo 'Format: php convert.php putty.reg shuttle.json' . PHP_EOL;
+    echo 'Format: php convert.php putty.reg ssh_config' . PHP_EOL;
     exit( 1 );
-};
+}
 
 $src = $argv[1];
 $dst = $argv[2];
@@ -27,102 +25,75 @@ $lines = file( $src );
 // holds all sessions found.
 $list = array();
 
-// holds current session being processed in loop.
-$session = array();
+// sessions counter
+$i = 0;
 
-foreach ( $lines as $line )
-{
+foreach ( $lines as $line ) {
     $parts = str_split( $line );
 
     $str = '';
 
     // parse file to strip non-ASCII chars.
-    foreach ( $parts as $chr )
-    {
+    foreach ( $parts as $chr ) {
         $ord = ord( $chr );
 
         $str .= ( $ord ? $chr : '' );
-    };
+    }
 
     // denotes start of new session.
-    if ( preg_match( '/Sessions\\\(.*)\]/', $str, $matches ) )
-    {
-        if ( isset( $session['name'] ) )
-        {
-            // reorder the session names based on the hostname for uniformity.
-            if ( strpos( $session['name'], '@' ) !== FALSE )
-            {
-                list( $u, $h ) = explode( '@', $session['name'] );
-            };
-
-            // is the hostname provided as an IP address?
-            $ip = preg_match( '/\d+\.\d+\.\d+\.\d+/', $session['host'] );
-
-            $session['name'] = ( $ip ? $h : $session['host'] ) . ' - ' . $u;
-
-            $list[] = $session;
-        };
-
-        $session = array(
-            'name' => urldecode( $matches[1] ),
-            'host' => '',
-            'user' => ''
-        );
-    };
-
-    // parse hostname.
-    if ( preg_match( '/"HostName"="(.*)"/', $str, $matches ) )
-    {
-        #if ( strlen( $matches[1] ) === 0 ) continue;
-
-        $session['host'] = $matches[1];
-        
-        if ( strpos( $session['host'], '@' ) !== FALSE )
-        {
-            list( $u, $h ) = explode( '@', $session['host'] );
-
-            $session['host'] = $h;
-        };
-    };
+    if ( preg_match( '/Sessions\\\(.*)\]/', $str, $matches ) ) {
+        $list[++$i] = array();
+        $list[$i]['Host'] = urldecode( $matches[1] );
+    }
 
     // parse username.
-    if ( preg_match( '/"UserName"="(.*)"/', $str, $matches ) )
-    {
-        #if ( strlen( $matches[1] ) === 0 ) continue;
+    if ( preg_match( '/"UserName"="(.*)"/', $str, $matches ) ) {
+        $list[$i]['User'] = $matches[1];
+    }
 
-        $session['user'] = $matches[1];
-    };
-};
+    // parse hostname.
+    if ( preg_match( '/"HostName"="(.*)"/', $str, $matches ) ) {
+        $list[$i]['HostName'] = $matches[1];
 
-// construct .shuttle.json configuration file.
-$shuttle = array(
-    '_comments' => array(
-        'Valid terminals include: \'Terminal.app\' or \'iTerm\'',
-        'Hosts will also be read from your ~/.ssh/config or /etc/ssh_config file, if available',
-        'For more information on how to configure, please see http://fitztrev.github.io/shuttle/'
-    ),
-    'terminal' => 'Terminal.app',
-    'launch_at_login' => FALSE,
-    'show_ssh_config_hosts' => TRUE,
-    'ssh_config_ignore_hosts' => array(),
-    'ssh_config_ignore_keywords'=> array(),
-    'hosts' => array()
-);
+        if ( strpos( $list[$i]['HostName'], '@' ) !== FALSE )
+        {
+            list( $u, $h ) = explode( '@', $list[$i]['HostName'] );
 
-// convert sessions to accounts in shuttle format.
-foreach ( $list as $item )
-{
-    $shuttle['hosts'][] = array( 
-        'name' => $item['name'], 
-        'cmd' => sprintf( 'ssh %s@%s', $item['user'], $item['host'] ) 
-    );
-};
+            $list[$i]['HostName'] = $h;
 
-// convert to JSON string.
-$shuttle_json = json_encode( $shuttle );
+            if( !isset($list[$i]['User']) && strpos( $u, ':' ) !== FALSE) {
+                list( $u, $p ) = explode( ':', $u );
+                $list[$i]['User'] = $u;
+            }
+        }
+    }
+
+    // parse port.
+    if ( preg_match( '/"PortNumber"="(.*)"/', $str, $matches ) ) {
+        $list[$i]['Port'] = hexdec($matches[1]);
+    }
+
+    // parse PublicKeyFile.
+    if ( preg_match( '/"PublicKeyFile"="(.*)"/', $str, $matches ) ) {
+        if(strlen($matches[1]) > 0) {
+            echo "WARNING: for host ".$list[$i]['Host']." copy key manually. ".$matches[1]."\n\n";
+        }
+    }
+
+}
+
+$config = "";
+foreach ($list as $session) {
+    $srv = "";
+    foreach ($session as $key => $value) {
+        if($key != "Host") $srv .= "\t";
+        $srv .= sprintf("%s %s\n", $key, $value);
+    }
+    $config .= $srv."\n";
+}
 
 // write the file to the $dst.
-$bytes = file_put_contents( $dst, $shuttle_json );
+$bytes = file_put_contents( $dst, $config );
 
 echo sprintf( 'Completed conversion. %d bytes writtent to %s.', $bytes, $dst ) . PHP_EOL;
 ?>
